003cf31ec6d3e01bc58be65d8b96c9154689c3b8
Caroline Hertel <caroline.hertel@di-unternehmer.com>
2024-02-12 08:29:29 +0100

    update error-handling for people search

--- src/app/star-wars/people/people.component.ts
+++ src/app/star-wars/people/people.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgForm } from "@angular/forms";
-import { StarWarsService, StarWarsTopic } from "../../services/star-wars.service";
-import { PeopleInterface } from "./people.interface";
+import { NgForm } from '@angular/forms';
+import { StarWarsService, StarWarsTopic } from '../../services/star-wars.service';
+import { PeopleInterface } from './people.interface';
 
 @Component({
   selector: 'app-people',
@@ -12,16 +12,22 @@ export class PeopleComponent {
   @ViewChild('f') searchForm: NgForm
   resultList$: PeopleInterface[]
 
-
-  constructor (private starWarsService: StarWarsService) {}
+  constructor (private starWarsService: StarWarsService ) {}
 
   onSubmit (){
-    console.log(this.searchForm.value.search)
     this.starWarsService
       .getSearchedItem(StarWarsTopic.People, this.searchForm.value.search)
-      .subscribe((resultList) => {
-        this.resultList$ = <PeopleInterface[]>resultList
-        console.log(this.resultList$)
+      .subscribe({
+        next: (resultList) => {
+          this.resultList$ = <PeopleInterface[]>resultList
+        },
+        error: (error) => {
+          alert('Oops, something went wrong!')
+          this.searchForm.reset()
+        },
+        complete: () => {
+          this.searchForm.reset()
+        }
       })
   }
 
